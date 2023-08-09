41d425b6175ad374c4dad92a7745bda47919943a
Antonio Pedro <tonio.pedro99@gmail.com>
2023-08-09 20:49:16 +0530

    feat: use / commands

--- src/github.py
+++ src/github.py
@@ -1,13 +1,21 @@
-from datetime import datetime, timedelta
 from aiohttp import ClientSession
-from models.issue import GoodFirstIssue
-from typing import List
+from typing import List, Optional
 import logging
+from pydantic import BaseModel
 
 logging.basicConfig(level=logging.INFO)
 logging.basicConfig(level=logging.WARNING)
 logging.basicConfig(level=logging.ERROR)
 logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+class GoodFirstIssue(BaseModel):
+    """Model to represent a good first issue"""
+    title: str
+    html_url: Optional[str] = None
+    state: Optional[str] = None
+    issue_number: Optional[int] = None
+    created_at: Optional[str] = None
+    user: Optional[dict] = None
 
 class GithubSearcher:
     """Search for good first issues on github"""
@@ -15,19 +23,23 @@ class GithubSearcher:
     def __init__(self, github_token: str):
         self.github_token = github_token
         self.headers = {'Authorization': f'token {self.github_token}'}
-        self.params = {'q': f'is:issue is:open label:"good first issue" created:>{self.current_date()}', 
-                       'sort': 'updated', 
-                       'order': 'asc',
-                       'per_page': 5}
         self.url = 'https://api.github.com/search/issues'
 
     
-    async def search(self) -> List[GoodFirstIssue]:
+    async def search(self, q) -> List[GoodFirstIssue]:
         """Search for good first issues on github"""
         results = []
+
         async with ClientSession() as session:
             logging.info("Searching for new issues")
-            async with session.get(self.url, headers=self.headers, params=self.params) as response:
+            
+            params = {
+                'q': f'is:issue is:open label:"good first issue" {q}',
+                'sort': 'updated', 
+                'order': 'asc',
+                'per_page': 5}
+            
+            async with session.get(self.url, headers=self.headers, params=params) as response:
                 if response.status == 200:
                     logging.info("search done, parsing data")
                     data = await response.json()
@@ -39,7 +51,3 @@ class GithubSearcher:
                     logging.error("Error while searching issues")
 
         return results
-
-    def current_date(self):
-        """Return the current date minus 7 days"""
-        return (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
